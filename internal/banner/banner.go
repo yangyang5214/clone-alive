df9c480b9fff7922d8d041b7cf3547ee161d90ad
beer <beer5214@126.com>
2023-02-14 13:50:58 +0800

    releases v0.1.2

--- internal/banner/banner.go
+++ internal/banner/banner.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	Version     = "v0.1.1"
+	Version     = "v0.1.2"
 	ProjectName = "clone-alive"
 	//https://springhow.com/spring-boot-banner-generator/
 	banner = "       _                          _ _\n   ___| | ___  _ __   ___    __ _| (_)_   _____\n  / __| |/ _ \\| '_ \\ / _ \\  / _` | | \\ \\ / / _ \\\n | (__| | (_) | | | |  __/ | (_| | | |\\ V /  __/\n  \\___|_|\\___/|_| |_|\\___|  \\__,_|_|_| \\_/ \\___|"
